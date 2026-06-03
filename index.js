0e46364421a5be906516a95e842acdd77cee951f
paraspathania <paraspathania705@gmail.com>
2026-06-03 22:30:01 +0530

    Testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 5500;
 
 app.get('/', (req, res) => {
-    res.send("Hello World! ");
+    res.send("Hello World This is a simple Node.js app running in a Docker container! ");
 });
 
 app.listen(PORT, () => {
